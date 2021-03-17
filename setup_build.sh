1537d9a7a4b00b3d0b81de7fc50dce12287f6dd9
pabloinigoblasco <pablo@ibrobotics.com>
2021-03-17 23:31:26 +0100

    ci

--- setup_build.sh
+++ setup_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install xterm python3-rosdistro python3-rosdep2
+sudo apt-get install xterm python3-rosdistro python3-rosdep
 sudo pip3 install -U vcstool
 
 echo "assumming we are located in the smacc folder"
@@ -11,4 +11,4 @@ fi
 cd dependencies
 /usr/bin/vcs import < ../submodules/navigation2/tools/underlay.repos 
 cd ..
-/usr/bin/rosdep install -q -y --from-paths . --skip-keys "slam_toolbox"
+/usr/bin/rosdep install -q -y --from-paths . --skip-keys "slam_toolbox" > /dev/null
