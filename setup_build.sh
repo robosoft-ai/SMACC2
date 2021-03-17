fb8d438ec50246296a5d839e5e505dafdc582f03
pabloinigoblasco <pablo@ibrobotics.com>
2021-03-17 22:54:08 +0100

    setup_build.sh

--- setup_build.sh
+++ setup_build.sh
@@ -1,9 +1,10 @@
 sudo apt-get install xterm
 echo "assumming we are located in the smacc folder"
+if [ ! -d dependencies ] mkdir dependencies
 cd dependencies
 vcs import < ../submodules/navigation2/tools/underlay.repos 
 cd ..
 rosdep install -q -y --from-paths . --skip-keys "slam_toolbox"
 
 
-sudo apt-get install ros-rolling-cyclonedds ros-rolling-rmw-cyclonedds-cpp
+#sudo apt-get install ros-rolling-cyclonedds ros-rolling-rmw-cyclonedds-cpp
