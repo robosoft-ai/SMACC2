346c7f16e395be9f90e06d0fed2712f6b4d8ede7
pabloinigoblasco <pablo@ibrobotics.com>
2021-03-25 19:48:34 +0100

    setup build

--- setup_build.sh
+++ setup_build.sh
@@ -6,20 +6,17 @@ sudo pip3 install -U vcstool
 
 echo "installing ros2 repos"
 
+if [ ! -d ros2_repos ] 
+then
+mkdir ros2_repos 
+fi
+
 wget https://raw.githubusercontent.com/ros2/ros2/foxy/ros2.repos
-mkdir ros2_repos
+
 vcs import ros2_repos < ros2.repos
 vcs pull ros2_repos
 
 echo "assumming we are located in the smacc folder"
-if [ ! -d dependencies ] 
-then
-mkdir dependencies 
-fi
-cd dependencies
-mkdir ros2
 
-vcs import < ../submodules/navigation2/tools/underlay.repos 
-cd ..
-#rosdep update > /dev/null
-#rosdep install -q -y --from-paths submodules/navigation2/ --skip-keys "slam_toolbox"
+vcs import ros2_repos < ../submodules/navigation2/tools/underlay.repos 
+
