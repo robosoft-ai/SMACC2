a35ba0e86b296b706240e060fdd237f9dd52e430
pabloinigoblasco <pablo@ibrobotics.com>
2021-04-28 21:08:30 +0200

    preparing foxy branch

--- smacc_sm_reference_library/sm_dance_bot/src/sm_dance_bot/sm_dance_bot.cpp
+++
@@ -1,7 +0,0 @@
-#include <sm_dance_bot/sm_dance_bot.h>
-
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    smacc::run<SmDanceBot>();
-}
--- smacc_sm_reference_library/sm_dance_bot_strikes_back/src/sm_dance_bot_strikes_back/sm_dance_bot_strikes_back.cpp
+++
@@ -1,6 +0,0 @@
-#include <sm_dance_bot_strikes_back/sm_dance_bot_strikes_back.h>
-int main(int argc, char **argv)
-{
-  rclcpp::init(argc, argv);
-  smacc::run<SmDanceBotStrikesBack>();
-}
