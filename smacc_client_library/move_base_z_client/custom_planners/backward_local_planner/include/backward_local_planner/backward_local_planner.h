39c7a0983896ef5abd445baee0e5c8aa4e88d738
Denis Štogl <denis@stogl.de>
2021-08-18 11:35:03 +0200

    Resolve missing dependcy in smacc_msgs and reorganize them for better overview.

--- smacc_client_library/move_base_z_client/custom_planners/backward_local_planner/include/backward_local_planner/backward_local_planner.h
+++ smacc_client_library/move_base_z_client/custom_planners/backward_local_planner/include/backward_local_planner/backward_local_planner.h
@@ -21,6 +21,7 @@
 //#include <dynamic_reconfigure/server.h>
 //#include <backward_local_planner/BackwardLocalPlannerConfig.h>
 #include <tf2/transform_datatypes.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2/utils.h>
 #include <tf2_ros/buffer.h>
 
