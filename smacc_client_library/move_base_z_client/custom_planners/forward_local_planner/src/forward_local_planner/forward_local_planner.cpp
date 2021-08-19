cb1e2f28474e9e82c7e23e915c281e2392a0757f
Denis Štogl <denis@stogl.de>
2021-08-19 23:20:42 +0200

    Remove some deprication and update style of repos files.

--- smacc_client_library/move_base_z_client/custom_planners/forward_local_planner/src/forward_local_planner/forward_local_planner.cpp
+++ smacc_client_library/move_base_z_client/custom_planners/forward_local_planner/src/forward_local_planner/forward_local_planner.cpp
@@ -628,7 +628,7 @@ void ForwardLocalPlanner::setPlan(const nav_msgs::msg::Path & plan)
 {
   nav_msgs::msg::Path transformedPlan;
 
-  rclcpp::Duration ttol(transform_tolerance_);
+  rclcpp::Duration ttol = rclcpp::Duration::from_seconds(transform_tolerance_);
   // transform global plan
   for (auto & p : plan.poses)
   {
