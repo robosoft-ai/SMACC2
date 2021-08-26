be95ba029c5575388e3137d97f8932b906ac84f6
Denis Štogl <denis@stogl.de>
2021-08-26 21:45:52 +0200

    Correct formatters.

--- smacc_client_library/move_base_z_client/custom_planners/backward_local_planner/include/backward_local_planner/backward_local_planner.h
+++ smacc_client_library/move_base_z_client/custom_planners/backward_local_planner/include/backward_local_planner/backward_local_planner.h
@@ -21,9 +21,9 @@
 //#include <dynamic_reconfigure/server.h>
 //#include <backward_local_planner/BackwardLocalPlannerConfig.h>
 #include <tf2/transform_datatypes.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2/utils.h>
 #include <tf2_ros/buffer.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <Eigen/Eigen>
 #include <geometry_msgs/msg/pose_stamped.hpp>
