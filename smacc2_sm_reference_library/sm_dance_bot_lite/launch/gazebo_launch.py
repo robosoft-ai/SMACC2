e81d5a32d9cd3db77d0a711d7269263cf11fb9d5
Pablo Iñigo Blasco <pablo@ibrobotics.com>
2021-10-05 23:07:12 +0200

    Feature/sm dance bot lite gazebo fixes (#104)
    * sm_dance_bot visualizing turtlebot3
    
    * cleaning and lidar show/hide option
    
    * cleaning files and making formating work
    
    * more fixes
    
    * gazebo fixes, to show the robot and the lidar
    
    * format fixes

--- smacc2_sm_reference_library/sm_dance_bot_lite/launch/gazebo_launch.py
+++ smacc2_sm_reference_library/sm_dance_bot_lite/launch/gazebo_launch.py
@@ -18,7 +18,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration, PythonExpression
 
 
@@ -30,6 +30,7 @@ def generate_launch_description():
     use_simulator = LaunchConfiguration("use_simulator")
     world = LaunchConfiguration("world")
     headless = LaunchConfiguration("headless")
+    show_gz_lidar = LaunchConfiguration("show_gz_lidar")
 
     sm_dance_bot_lite_dir = get_package_share_directory("sm_dance_bot_lite")
     launch_dir = os.path.join(sm_dance_bot_lite_dir, "launch")
@@ -42,16 +43,26 @@ def generate_launch_description():
         "headless", default_value="False", description="Whether to execute gzclient)"
     )
 
+    declare_show_gz_lidar = DeclareLaunchArgument(
+        "show_gz_lidar",
+        default_value="True",
+        description="Whether to apply a namespace to the navigation stack",
+    )
+
     declare_world_cmd = DeclareLaunchArgument(
         "world",
-        # TODO(orduno) Switch back once ROS argument passing has been fixed upstream
-        #              https://github.com/ROBOTIS-GIT/turtlebot3_simulations/issues/91
-        # default_value=os.path.join(get_package_share_directory('turtlebot3_gazebo'),
-        #                            'worlds/turtlebot3_worlds/waffle.model'),
-        # default_value=os.path.join(sm_dance_bot_lite_dir, 'worlds', 'waffle.model'),
-        # default_value=os.path.join(sm_dance_bot_lite_dir, 'worlds', 'sm_dance_bot_lite_world.model'),
         default_value=os.path.join(sm_dance_bot_lite_dir, "worlds", "ridgeback_race.world"),
         description="Full path to world model file to load",
+        condition=IfCondition(show_gz_lidar),
+    )
+
+    declare_world_cmd_2 = DeclareLaunchArgument(
+        "world",
+        default_value=os.path.join(
+            sm_dance_bot_lite_dir, "worlds", "ridgeback_race_no_lidar.world"
+        ),
+        description="Full path to world model file to load",
+        condition=UnlessCondition(show_gz_lidar),
     )
 
     # Create the launch description and populate
@@ -60,11 +71,15 @@ def generate_launch_description():
     # xtermprefix = "xterm -xrm 'XTerm*scrollBar:  true' -xrm 'xterm*rightScrollBar: true' " \
     # "-hold -geometry 1000x600 -sl 10000 -e"
 
+    gzenv = dict(os.environ)
+    model_database_uri = os.environ["GAZEBO_MODEL_PATH"]
+    gzenv["GAZEBO_MODEL_DATABASE_URI"] = model_database_uri
+
     # Specify the actions
     start_gazebo_server_cmd = ExecuteProcess(
         condition=IfCondition(use_simulator),
         cmd=["gzserver", "-s", "libgazebo_ros_init.so", world, "--verbose"],
-        # env={"GAZEBO_MODEL_PATH": os.getcwd(),"HOME": os.environ["HOME"]},
+        env=gzenv,
         cwd=[launch_dir],
         output="screen",
     )
@@ -73,6 +88,7 @@ def generate_launch_description():
         condition=IfCondition(PythonExpression([use_simulator, " and not ", headless])),
         cmd=["gzclient"],
         cwd=[launch_dir],
+        env=gzenv,
         output="screen",
     )
 
@@ -80,6 +96,9 @@ def generate_launch_description():
     ld.add_action(declare_simulator_cmd)
     ld.add_action(declare_use_simulator_cmd)
     ld.add_action(declare_world_cmd)
+    ld.add_action(declare_world_cmd_2)
+
+    ld.add_action(declare_show_gz_lidar)
 
     ld.add_action(start_gazebo_server_cmd)
     ld.add_action(start_gazebo_client_cmd)
