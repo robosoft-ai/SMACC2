17e92fe6e70468de373fb37802a3126d35b56fc2
pabloinigoblasco <pablo@ibrobotics.com>
2021-10-05 21:35:04 +0200

    cleaning and lidar show/hide option

--- smacc2_sm_reference_library/sm_dance_bot/launch/gazebo_launch.py
+++ smacc2_sm_reference_library/sm_dance_bot/launch/gazebo_launch.py
@@ -16,9 +16,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription
+from launch import LaunchDescription, condition
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration, PythonExpression
 
 
@@ -30,7 +30,8 @@ def generate_launch_description():
     use_simulator = LaunchConfiguration("use_simulator")
     world = LaunchConfiguration("world")
     headless = LaunchConfiguration("headless")
-
+    show_gz_lidar = LaunchConfiguration("show_gz_lidar")
+    
     sm_dance_bot_dir = get_package_share_directory("sm_dance_bot")
     launch_dir = os.path.join(sm_dance_bot_dir, "launch")
 
@@ -42,18 +43,27 @@ def generate_launch_description():
         "headless", default_value="False", description="Whether to execute gzclient)"
     )
 
-    declare_world_cmd = DeclareLaunchArgument(
-        "world",
-        # TODO(orduno) Switch back once ROS argument passing has been fixed upstream
-        #              https://github.com/ROBOTIS-GIT/turtlebot3_simulations/issues/91
-        # default_value=os.path.join(get_package_share_directory('turtlebot3_gazebo'),
-        #                            'worlds/turtlebot3_worlds/waffle.model'),
-        # default_value=os.path.join(sm_dance_bot_dir, 'worlds', 'waffle.model'),
-        # default_value=os.path.join(sm_dance_bot_dir, 'worlds', 'sm_dance_bot_world.model'),
-        default_value=os.path.join(sm_dance_bot_dir, "worlds", "ridgeback_race.world"),
-        description="Full path to world model file to load",
+    declare_show_gz_lidar = DeclareLaunchArgument(
+        "show_gz_lidar",
+        default_value="True",
+        description="Whether to apply a namespace to the navigation stack",
     )
 
+    declare_world_cmd = DeclareLaunchArgument(
+        "world",
+        default_value=os.path.join(sm_dance_bot_dir, "worlds", "ridgeback_race.world"),
+        description="Full path to world model file to load",
+        condition = IfCondition(show_gz_lidar)
+    )
+
+    declare_world_cmd_2 = DeclareLaunchArgument(
+        "world",
+        default_value=os.path.join(sm_dance_bot_dir, "worlds", "ridgeback_race_no_lidar.world"),
+        description="Full path to world model file to load",
+        condition = UnlessCondition(show_gz_lidar)
+    )
+
+    
     # Create the launch description and populate
     ld = LaunchDescription()
 
@@ -85,7 +95,10 @@ def generate_launch_description():
     ld.add_action(declare_simulator_cmd)
     ld.add_action(declare_use_simulator_cmd)
     ld.add_action(declare_world_cmd)
-
+    ld.add_action(declare_world_cmd_2)
+    
+    ld.add_action(declare_show_gz_lidar)
+    
     ld.add_action(start_gazebo_server_cmd)
     ld.add_action(start_gazebo_client_cmd)
 
