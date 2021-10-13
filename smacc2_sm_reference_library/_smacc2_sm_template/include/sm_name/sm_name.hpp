de0694a6022ac6e29c0c968ab4a69953e8f707d8
Denis Štogl <denis@stogl.de>
2021-10-13 11:23:29 +0200

    First working version of sm template and template generator.

---
+++ smacc2_sm_reference_library/_smacc2_sm_template/include/sm_name/sm_name.hpp
@@ -0,0 +1,54 @@
+// Copyright 2021 MyName/MyCompany Inc.
+// Copyright 2021 RobosoftAI Inc. (template)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+// Author: Denis Štogl (template)
+//
+
+#pragma once
+
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "smacc2/smacc.hpp"
+
+namespace $sm_name$
+{
+//STATES
+struct State1;
+struct State2;
+
+//VARIABLES - shared between states (using "_<name>_"-syntax to make this obvious)
+static unsigned int _counter_ = 0;
+std::shared_ptr<rclcpp::Node> _node_;
+rclcpp::Time _start_time_;
+
+//--------------------------------------------------------------------
+//STATE_MACHINE
+struct $SmName$
+: public smacc2::SmaccStateMachineBase<$SmName$, State1>
+{
+  using SmaccStateMachineBase::SmaccStateMachineBase;
+
+  void onInitialize() override {
+    _node_ = std::make_shared<rclcpp::Node>("$sm_name$");
+    _start_time_ = _node_->now();
+  }
+};
+
+}  // namespace $sm_name$
+
+#include "states/st_state_1.hpp"
+#include "states/st_state_2.hpp"
