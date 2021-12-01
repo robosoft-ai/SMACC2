c080bf20618d305a4ace595083c7c4a9afd7d91b
Denis Štogl <denis@stogl.de>
2021-12-01 21:35:33 +0100

    Add Hercules test files.

---
+++ smacc2_sm_reference_library/sm_hercules_1/README.md
@@ -0,0 +1,98 @@
+ <h2>Build Instructions</h2>
+
+First, source your chosen ros2 distro.
+```
+source /opt/ros/rolling/setup.bash
+```
+```
+source /opt/ros/galactic/setup.bash
+```
+
+Before you build, make sure you've installed all the dependencies...
+
+```
+rosdep install --ignore-src --from-paths src -y -r
+```
+
+Then build with colcon build...
+
+```
+colcon build
+```
+ <h2>Operating Instructions</h2>
+After you build, remember to source the proper install folder...
+
+```
+source ~/<ros2_ws>/install/setup.bash
+```
+
+And then run the launch file...
+
+```
+ros2 launch sm_hercules_1 sm_hercules_1.launch
+```
+
+ <h2>Viewer Instructions</h2>
+If you have the SMACC2 Runtime Analyzer installed then type...
+
+```
+ros2 run smacc2_rta smacc2_rta
+```
+
+If you don't have the SMACC2 Runtime Analyzer click <a href="https://robosoft.ai/product-category/smacc2-runtime-analyzer/">here</a>
+
+
+ <h2>Hercules testing instructions</h2>
+
+"Hercules" robot consists of Husky mobile base and two UR manipulators.
+
+To start simulation with test setup start the following commands.
+
+
+ <h3>Dual UR manipulators</h3>
+
+```
+ros2 launch sm_hercules_1 dual_ur_sim_control.launch.py
+```
+
+Start test script to see arm movement with the following command (wait 6 seconds until arm start to move):
+
+```
+ros2 launch sm_hercules_1 test_dual_joint_trajectory_controller.launch.py
+```
+
+ <h3>Hercules</h3>
+
+```
+ros2 launch sm_hercules_1 hercules_sim_control.launch.py
+```
+
+Start test script to see arm movement with the following command (wait 6 seconds until arm start to move):
+
+```
+ros2 launch sm_hercules_1 test_dual_joint_trajectory_controller.launch.py
+```
+
+Send command for the base using:
+
+```
+ros2 topic pub /base_velocity_controller/cmd_vel_unstamped geometry_msgs/msg/Twist "linear:
+  x: 0.1
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.5"
+```
+
+
+  <h3> Running Ignition simulation </h3>
+
+To run simulation in Ignition add the following arguments to launch files:
+
+```
+gazebo_sim:=false ignition_sim:=true
+```
+
+**NOTE:** Hercules is currently not working in Ignition, but only in Gazebo.
---
+++ smacc2_sm_reference_library/sm_hercules_1/launch/examples/hercules_sim_control.launch.py
@@ -0,0 +1,400 @@
+# Copyright (c) 2021 Stogl Robotics Consulting UG (haftungsbeschränkt)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Denis Stogl
+
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+# For port/starboard nomenclature see: https://en.wikipedia.org/wiki/Port_and_starboard
+
+
+def launch_setup(context, *args, **kwargs):
+
+    # Simulation arguments
+    gazebo_sim = LaunchConfiguration("gazebo_sim")
+    ignition_sim = LaunchConfiguration("ignition_sim")
+    # Initialize Arguments
+    name = LaunchConfiguration("name")
+    ur_type = LaunchConfiguration("ur_type")
+    safety_limits = LaunchConfiguration("safety_limits")
+    safety_pos_margin = LaunchConfiguration("safety_pos_margin")
+    safety_k_position = LaunchConfiguration("safety_k_position")
+    # General arguments
+    runtime_config_package = LaunchConfiguration("runtime_config_package")
+    controllers_file = LaunchConfiguration("controllers_file")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    prefix = LaunchConfiguration("prefix")
+    start_arm_controllers = LaunchConfiguration("start_arm_controllers")
+    base_initial_controller = LaunchConfiguration("base_initial_controller")
+    port_initial_joint_controller = LaunchConfiguration("port_initial_joint_controller")
+    starboard_initial_joint_controller = LaunchConfiguration("starboard_initial_joint_controller")
+    launch_rviz = LaunchConfiguration("launch_rviz")
+
+    joint_limit_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", ur_type, "joint_limits.yaml"]
+    )
+    kinematics_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", ur_type, "default_kinematics.yaml"]
+    )
+    physical_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", ur_type, "physical_parameters.yaml"]
+    )
+    visual_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", ur_type, "visual_parameters.yaml"]
+    )
+
+    initial_joint_controllers = PathJoinSubstitution(
+        [FindPackageShare(runtime_config_package), "config", "examples", controllers_file]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
+    )
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "urdf", description_file]
+            ),
+            " ",
+            "name:=",
+            name,
+            " ",
+            "joint_limit_params:=",
+            joint_limit_params,
+            " ",
+            "kinematics_params:=",
+            kinematics_params,
+            " ",
+            "physical_params:=",
+            physical_params,
+            " ",
+            "visual_params:=",
+            visual_params,
+            " ",
+            "safety_limits:=",
+            safety_limits,
+            " ",
+            "safety_pos_margin:=",
+            safety_pos_margin,
+            " ",
+            "safety_k_position:=",
+            safety_k_position,
+            " ",
+            "name:=",
+            ur_type,
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+            "gazebo_sim:=",
+            gazebo_sim,
+            " ",
+            "ignition_sim:=",
+            ignition_sim,
+            " ",
+            "simulation_controllers:=",
+            initial_joint_controllers,
+            # husky parameters
+            " ",
+            "is_sim:=true",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[{"use_sim_time": True}, robot_description],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(launch_rviz),
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    # Base
+    spawn_base_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[base_initial_controller, "-c", "/controller_manager"],
+        output="screen",
+    )
+
+    # Port
+
+    # There may be other controllers of the joints, but this is the initially-started one
+    port_initial_joint_controller_spawner_started = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[port_initial_joint_controller, "-c", "/controller_manager"],
+        condition=IfCondition(start_arm_controllers),
+    )
+    port_initial_joint_controller_spawner_stopped = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[port_initial_joint_controller, "-c", "/controller_manager", "--stopped"],
+        condition=UnlessCondition(start_arm_controllers),
+    )
+
+    # Starboard
+
+    # There may be other controllers of the joints, but this is the initially-started one
+    starboard_initial_joint_controller_spawner_started = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[starboard_initial_joint_controller, "-c", "/controller_manager"],
+        condition=IfCondition(start_arm_controllers),
+    )
+    starboard_initial_joint_controller_spawner_stopped = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[starboard_initial_joint_controller, "-c", "/controller_manager", "--stopped"],
+        condition=UnlessCondition(start_arm_controllers),
+    )
+
+    # Gazebo nodes
+    gzserver = ExecuteProcess(
+        cmd=["gzserver", "-s", "libgazebo_ros_init.so", "-s", "libgazebo_ros_factory.so", ""],
+        output="screen",
+        condition=IfCondition(gazebo_sim),
+    )
+
+    # Gazebo client
+    gzclient = ExecuteProcess(
+        cmd=["gzclient"],
+        output="screen",
+        condition=IfCondition(gazebo_sim),
+    )
+
+    # Spawn robot
+    gazebo_spawn_robot = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        name="spawn_ur",
+        arguments=["-entity", name, "-topic", "robot_description"],
+        output="screen",
+        condition=IfCondition(gazebo_sim),
+    )
+
+    # Ignition nodes
+    ignition_spawn_entity = Node(
+        package="ros_ign_gazebo",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-string",
+            robot_description_content,
+            "-name",
+            name,
+            "-allow_renaming",
+            "true",
+        ],
+        condition=IfCondition(ignition_sim),
+    )
+
+    ignition_launch_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("ros_ign_gazebo"), "/launch/ign_gazebo.launch.py"]
+        ),
+        launch_arguments={"ign_args": " -r -v 3 empty.sdf"}.items(),
+        condition=IfCondition(ignition_sim),
+    )
+
+    nodes_to_start = [
+        robot_state_publisher_node,
+        rviz_node,
+        joint_state_broadcaster_spawner,
+        spawn_base_controller,
+        port_initial_joint_controller_spawner_stopped,
+        port_initial_joint_controller_spawner_started,
+        starboard_initial_joint_controller_spawner_stopped,
+        starboard_initial_joint_controller_spawner_started,
+        gzserver,
+        gzclient,
+        gazebo_spawn_robot,
+        ignition_spawn_entity,
+        ignition_launch_description,
+    ]
+
+    return nodes_to_start
+
+
+def generate_launch_description():
+    declared_arguments = []
+    # Simulation specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gazebo_sim",
+            default_value="true",
+            description="Use Gazebo Classic for simulation",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ignition_sim",
+            default_value="false",
+            description="Use Ignition for simulation",
+        )
+    )
+    # UR specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "name",
+            description="Robot name",
+            default_value="hercules",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ur_type",
+            description="Type/series of used UR robot.",
+            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e"],
+            default_value="ur5e",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_limits",
+            default_value="true",
+            description="Enables the safety limits controller if true.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_pos_margin",
+            default_value="0.15",
+            description="The margin to lower and upper limits in the safety controller.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_k_position",
+            default_value="20",
+            description="k-position factor in the safety controller.",
+        )
+    )
+    # General arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "runtime_config_package",
+            default_value="sm_hercules_1",
+            description='Package with the controller\'s configuration in "config" folder. \
+        Usually the argument is not set, it enables use of a custom setup.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="hercules_controllers.yaml",
+            description="YAML file with the controllers configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="sm_hercules_1",
+            description="Description package with robot URDF/XACRO files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="hercules.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "fake_sensor_commands",
+            default_value="false",
+            description="Enable fake command interfaces for sensors used for simple simulations. \
+            Used only if 'use_fake_hardware' parameter is true.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "start_arm_controllers",
+            default_value="true",
+            description="Start arom controllers",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "base_initial_controller",
+            default_value="base_velocity_controller",
+            description="Base controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "port_initial_joint_controller",
+            default_value="port_joint_trajectory_controller",
+            description="Robot controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "starboard_initial_joint_controller",
+            default_value="starboard_joint_trajectory_controller",
+            description="Robot controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
