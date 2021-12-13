0e847838977e679a3d71ae185244dae8b57f2ee3
reelrbtx <brett2@reelrobotics.com>
2021-12-13 10:43:37 -0800

    sm_dance_bot_warehouse_3 (#181)
    Co-authored-by: Ubuntu 20-04-02-amd64 <brett@robosoft.ai>

---
+++ smacc2_sm_reference_library/sm_dance_bot_warehouse_3/include/sm_dance_bot_warehouse_3/orthogonals/or_temperature_sensor.hpp
@@ -0,0 +1,43 @@
+// Copyright 2021 RobosoftAI Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*****************************************************************************************************************
+ *
+ * 	 Authors: Pablo Inigo Blasco, Brett Aldrich
+ *
+ ******************************************************************************************************************/
+
+#pragma once
+
+#include <multirole_sensor_client/cl_multirole_sensor.hpp>
+#include <sensor_msgs/msg/temperature.hpp>
+#include <sm_dance_bot_warehouse_3/clients/cl_temperature_sensor/cl_temperature_sensor.hpp>
+#include <smacc2/smacc_orthogonal.hpp>
+
+using namespace std::chrono_literals;
+
+namespace sm_dance_bot_warehouse_3
+{
+class OrTemperatureSensor : public smacc2::Orthogonal<OrTemperatureSensor>
+{
+public:
+  void onInitialize() override
+  {
+    auto clTemperatureSensor = this->createClient<ClTemperatureSensor>();
+
+    clTemperatureSensor->topicName = "/temperature";
+    clTemperatureSensor->timeout_ = rclcpp::Duration(10s);
+  }
+};
+}  // namespace sm_dance_bot_warehouse_3
---
+++ smacc2_sm_reference_library/sm_dance_bot_warehouse_3/include/sm_dance_bot_warehouse_3/states/radial_motion_states/sti_radial_loop_start.hpp
@@ -0,0 +1,59 @@
+// Copyright 2021 RobosoftAI Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*****************************************************************************************************************
+ *
+ * 	 Authors: Pablo Inigo Blasco, Brett Aldrich
+ *
+ ******************************************************************************************************************/
+
+namespace sm_dance_bot_warehouse_3
+{
+namespace radial_motion_states
+{
+// STATE DECLARATION
+struct StiRadialLoopStart : smacc2::SmaccState<StiRadialLoopStart, SS>
+{
+  using SmaccState::SmaccState;
+
+  // TRANSITION TABLE
+  typedef mpl::list<
+
+    Transition<EvLoopContinue<StiRadialLoopStart>, StiRadialRotate, CONTINUELOOP>
+
+    >reactions;
+
+  // STATE FUNCTIONS
+  static void staticConfigure() {}
+
+  void runtimeConfigure() {}
+
+  bool loopWhileCondition()
+  {
+    auto & superstate = this->context<SS>();
+
+    RCLCPP_INFO(
+      getLogger(), "Loop start, current iterations: %d, total iterations: %d",
+      superstate.iteration_count, superstate.total_iterations());
+    return superstate.iteration_count++ < superstate.total_iterations();
+  }
+
+  void onEntry()
+  {
+    RCLCPP_INFO(getLogger(), "LOOP START ON ENTRY");
+    checkWhileLoopConditionAndThrowEvent(&StiRadialLoopStart::loopWhileCondition);
+  }
+};
+}  // namespace radial_motion_states
+}  // namespace sm_dance_bot_warehouse_3
