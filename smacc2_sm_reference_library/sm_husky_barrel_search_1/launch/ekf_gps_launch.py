86f93d18fc4ef06c31c39b013ab1ee10bd097827
pabloinigoblasco <pablo@ibrobotics.com>
2022-04-25 23:18:55 +0200

    minor

--- smacc2_sm_reference_library/sm_husky_barrel_search_1/launch/ekf_gps_launch.py
+++ smacc2_sm_reference_library/sm_husky_barrel_search_1/launch/ekf_gps_launch.py
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ast import arguments
 from launch import LaunchDescription
 import launch_ros.actions
 import os
