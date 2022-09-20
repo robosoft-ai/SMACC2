3e5cbf5443028496dcc6cfe96d455adf79da586b
Manuel M <mamueluth@gmail.com>
2022-09-20 15:54:24 +0200

    make SMACC2 compile on humble

--- smacc2_client_library/nav2z_client/custom_planners/backward_local_planner/include/backward_local_planner/backward_local_planner.hpp
+++ smacc2_client_library/nav2z_client/custom_planners/backward_local_planner/include/backward_local_planner/backward_local_planner.hpp
@@ -26,6 +26,7 @@
 #include <nav2_core/controller.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
 
 #include <tf2/utils.h>
 #include <eigen3/Eigen/Eigen>
@@ -45,9 +46,9 @@ public:
   virtual ~BackwardLocalPlanner();
 
   void configure(
-    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent, std::string name,
-    const std::shared_ptr<tf2_ros::Buffer> & tf,
-    const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> & costmap_ros) override;
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent, 
+    std::string name, const std::shared_ptr<tf2_ros::Buffer> tf,
+    const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
 
   void activate() override;
 
--- smacc2_client_library/nav2z_client/custom_planners/forward_local_planner/include/forward_local_planner/forward_local_planner.hpp
+++ smacc2_client_library/nav2z_client/custom_planners/forward_local_planner/include/forward_local_planner/forward_local_planner.hpp
@@ -44,9 +44,9 @@ public:
   virtual ~ForwardLocalPlanner();
 
   void configure(
-    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent, std::string name,
-    const std::shared_ptr<tf2_ros::Buffer> & tf,
-    const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> & costmap_ros) override;
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+    std::string name, const std::shared_ptr<tf2_ros::Buffer> tf,
+    const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
 
   void activate() override;
   void deactivate() override;
