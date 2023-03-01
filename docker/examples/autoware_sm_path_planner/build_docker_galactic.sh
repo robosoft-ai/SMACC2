6c2295972d895892e0673e696601b65cb3d46b10
pabloinigoblasco <pablo@ibrobotics.com>
2023-03-01 19:10:33 +0100

    minor

--- docker/examples/autoware_sm_path_planner/build_docker_galactic.sh
+++ docker/examples/autoware_sm_path_planner/build_docker_galactic.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 DIR="$(dirname "$(realpath "$0")")"
 echo $DIR
-$DIR/../../build_docker.sh galactic galactic focal
+
+GIT_BRANCH=galactic
+ROS_DISTRO=galactic
+echo "ros distro: $ROS_DISTRO"
+echo "git branch: $GIT_BRANCH"
+which docker
+
+cd $DIR/../../..
+pwd
+
+docker build --build-arg ROS_DISTRO=$ROS_DISTRO --build-arg GIT_BRANCH=$GIT_BRANCH -t smacc2_autoware_sm_path_planner:$ROS_DISTRO -f docker/examples/autoware_sm_path_planner/Dockerfile .
+
+#build  -t smacc2:$ROS_DISTRO -f docker/Dockerfile .
