31564152f1c0cd24fef66ae9c52367a2930d2b24
pabloinigoblasco <pablo@ibrobotics.com>
2023-05-10 23:17:16 +0200

    refactoring folders

--- smacc2_dev_tools/docker/build_docker.sh
+++ smacc2_dev_tools/docker/build_docker.sh
@@ -10,4 +10,4 @@ NOCACHE="--no-cache"
 
 echo "ros distro: $ROS_DISTRO"
 echo "git branch: $GITBRANCH"
-sudo docker build --build-arg ROS_DISTRO=$ROS_DISTRO --build-arg GIT_BRANCH=$GIT_BRANCH -t smacc2:$ROS_DISTRO -f docker/Dockerfile . $NOCACHE
+sudo docker build --build-arg ROS_DISTRO=$ROS_DISTRO --build-arg GIT_BRANCH=$GIT_BRANCH -t smacc2:$ROS_DISTRO -f docker/smacc2_dev_tools/Dockerfile . $NOCACHE
