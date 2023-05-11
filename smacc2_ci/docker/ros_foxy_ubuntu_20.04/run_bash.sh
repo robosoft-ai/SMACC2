7bf03336c7d857e6f40fee77f58ea93b664f4f7a
Pablo Iñigo Blasco <pablo@ibrobotics.com>
2023-05-12 00:13:21 +0200

    missing files (#471)

--- smacc2_ci/docker/ros_foxy_ubuntu_20.04/run_bash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./build.sh
-xhost +
-sudo docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it smacc2_foxy_ubuntu_2004 bash
--- smacc2_ci/docker/ros_rollingAndGalactic_ubuntu_20.04/run_bashRolling.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-./buildRolling.sh
-xhost +
-sudo docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it smacc2_rolling_ubuntu_2004 bash
