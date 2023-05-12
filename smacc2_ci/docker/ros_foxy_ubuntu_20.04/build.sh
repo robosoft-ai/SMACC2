885062f1f6fbb65affe8b05f600349ffa8b5ac69
pabloinigoblasco <pablo@ibrobotics.com>
2023-05-12 22:49:03 +0200

    fixing foxy

--- smacc2_ci/docker/ros_foxy_ubuntu_20.04/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd ../../../..
-echo "Building DOCKER from directory `pwd`"
-sudo docker build -f $DIR/Dockerfile -t smacc2_foxy_ubuntu_2004 .
-cd $DIR
