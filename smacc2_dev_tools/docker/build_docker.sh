296fef84c53c2340edb66e0571ed10a2aafcc769
Pablo Iñigo Blasco <pablo@ibrobotics.com>
2023-05-13 00:33:10 +0200

    Update Dockerfile (#477)
    * Update Dockerfile
    
    * Update build_docker.sh

--- smacc2_dev_tools/docker/build_docker.sh
+++ smacc2_dev_tools/docker/build_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIR="$(dirname "$(realpath "$0")")"
 echo $DIR
-cd $DIR/../..
+cd $DIR/..
 echo `pwd`
 
 ROS_DISTRO=$1
@@ -10,6 +10,12 @@ UBUNTU_VERSION=$3
 NOCACHE="--no-cache"
 NOCACHE=
 
+ROOT_DIR=`realpath $DIR/../..`
+
 echo "ros distro: $ROS_DISTRO"
-echo "git branch: $GITBRANCH"
-sudo docker build --build-arg ROS_DISTRO=$ROS_DISTRO --build-arg GIT_BRANCH=$GIT_BRANCH -t smacc2:$ROS_DISTRO -f smacc2_dev_tools/docker/Dockerfile . $NOCACHE
+echo "git branch: $GIT_BRANCH"
+echo "ubuntu version: $UBUNTU_VERSION"
+echo "root path: $ROOT_DIR"
+
+cd $ROOT_DIR
+sudo docker build --build-arg ROS_DISTRO=$ROS_DISTRO --build-arg GIT_BRANCH=$GIT_BRANCH --build-arg UBUNTU_VERSION=$UBUNTU_VERSION -t smacc2:$ROS_DISTRO --progress string -f $ROOT_DIR/smacc2_dev_tools/docker/Dockerfile . $NOCACHE
