966ab3b40249d108018815bff9b8e0b446815c7c
Pablo Iñigo Blasco <pablo@ibrobotics.com>
2023-05-12 23:53:56 +0200

    Update build_docker.sh (#476)

--- smacc2_dev_tools/docker/build_docker.sh
+++ smacc2_dev_tools/docker/build_docker.sh
@@ -16,5 +16,5 @@ echo "ros distro: $ROS_DISTRO"
 echo "git branch: $GIT_BRANCH"
 echo "ubuntu version: $UBUNTU_VERSION"
 echo "root path: $ROOT_DIR"
-
+cd $ROOT_DIR
 sudo docker build --build-arg ROS_DISTRO=$ROS_DISTRO --build-arg GIT_BRANCH=$GIT_BRANCH --build-arg UBUNTU_VERSION=$UBUNTU_VERSION -t smacc2:$ROS_DISTRO -f $ROOT_DIR/smacc2_dev_tools/docker/Dockerfile . $NOCACHE
