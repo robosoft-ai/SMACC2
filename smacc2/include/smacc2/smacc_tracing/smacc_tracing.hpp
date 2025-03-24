1cfbc8dbf393d5649c0a9c720468f37b60772620
contradict <contradict@gmail.com>
2025-03-24 16:23:56 -0700

    Updates for tracing

--- smacc2/include/smacc2/smacc_tracing/smacc_tracing.hpp
+++ smacc2/include/smacc2/smacc_tracing/smacc_tracing.hpp
@@ -24,39 +24,39 @@
 extern "C"
 {
 #endif
-  DECLARE_TRACEPOINT(spinOnce)
+  _DECLARE_TRACEPOINT(spinOnce)
 
-  DECLARE_TRACEPOINT(smacc2_event, const char * event_type)
+  _DECLARE_TRACEPOINT(smacc2_event, const char * event_type)
 
-  DECLARE_TRACEPOINT(smacc2_state_update_start, const char * updatable_element_name)
+  _DECLARE_TRACEPOINT(smacc2_state_update_start, const char * updatable_element_name)
 
-  DECLARE_TRACEPOINT(smacc2_state_update_end, const char * updatable_element_name)
+  _DECLARE_TRACEPOINT(smacc2_state_update_end, const char * updatable_element_name)
 
-  DECLARE_TRACEPOINT(smacc2_state_onRuntimeConfigure_start, const char * state_name)
+  _DECLARE_TRACEPOINT(smacc2_state_onRuntimeConfigure_start, const char * state_name)
 
-  DECLARE_TRACEPOINT(smacc2_state_onRuntimeConfigure_end, const char * state_name)
+  _DECLARE_TRACEPOINT(smacc2_state_onRuntimeConfigure_end, const char * state_name)
 
-  DECLARE_TRACEPOINT(smacc2_state_onEntry_start, const char * state_name)
+  _DECLARE_TRACEPOINT(smacc2_state_onEntry_start, const char * state_name)
 
-  DECLARE_TRACEPOINT(smacc2_state_onEntry_end, const char * state_name)
+  _DECLARE_TRACEPOINT(smacc2_state_onEntry_end, const char * state_name)
 
-  DECLARE_TRACEPOINT(smacc2_state_onExit_start, const char * state_name)
+  _DECLARE_TRACEPOINT(smacc2_state_onExit_start, const char * state_name)
 
-  DECLARE_TRACEPOINT(smacc2_state_onExit_end, const char * state_name)
+  _DECLARE_TRACEPOINT(smacc2_state_onExit_end, const char * state_name)
 
-  DECLARE_TRACEPOINT(
+  _DECLARE_TRACEPOINT(
     smacc2_client_behavior_on_entry_start, const char * state_name, const char * orthogonal_name,
     const char * client_behavior_name)
 
-  DECLARE_TRACEPOINT(
+  _DECLARE_TRACEPOINT(
     smacc2_client_behavior_on_entry_end, const char * state_name, const char * orthogonal_name,
     const char * client_behavior_name)
 
-  DECLARE_TRACEPOINT(
+  _DECLARE_TRACEPOINT(
     smacc2_client_behavior_on_exit_start, const char * state_name, const char * orthogonal_name,
     const char * client_behavior_name)
 
-  DECLARE_TRACEPOINT(
+  _DECLARE_TRACEPOINT(
     smacc2_client_behavior_on_exit_end, const char * state_name, const char * orthogonal_name,
     const char * client_behavior_name)
 
--- smacc2/src/smacc2/smacc_tracing.cpp
+++ smacc2/src/smacc2/smacc_tracing.cpp
@@ -22,56 +22,56 @@
 
 #ifdef TRACETOOLS_LTTNG_ENABLED
 #include <smacc2/smacc_tracing/trace_provider.hpp>
-#define CONDITIONAL_TP(...) tracepoint(TRACEPOINT_PROVIDER, __VA_ARGS__)
+#define CONDITIONAL_TP(...) tracepoint(TRACETOOLS_TRACEPOINT_PROVIDER, __VA_ARGS__)
 #else
 #define CONDITIONAL_TP(...)
 #endif
 
-void TRACEPOINT(spinOnce) { CONDITIONAL_TP(spinOnce); }
+void TRACETOOLS_TRACEPOINT(spinOnce) { CONDITIONAL_TP(spinOnce); }
 
-void TRACEPOINT(smacc2_event, const char * event_type) { CONDITIONAL_TP(smacc2_event, event_type); }
+void TRACETOOLS_TRACEPOINT(smacc2_event, const char * event_type) { CONDITIONAL_TP(smacc2_event, event_type); }
 
-void TRACEPOINT(smacc2_state_update_start, const char * updatable_element_name)
+void TRACETOOLS_TRACEPOINT(smacc2_state_update_start, const char * updatable_element_name)
 {
   CONDITIONAL_TP(smacc2_state_update_start, updatable_element_name);
 }
 
-void TRACEPOINT(smacc2_state_update_end, const char * updatable_element_name)
+void TRACETOOLS_TRACEPOINT(smacc2_state_update_end, const char * updatable_element_name)
 {
   CONDITIONAL_TP(smacc2_state_update_end, updatable_element_name);
 }
 
-void TRACEPOINT(smacc2_state_onRuntimeConfigure_start, const char * state_name)
+void TRACETOOLS_TRACEPOINT(smacc2_state_onRuntimeConfigure_start, const char * state_name)
 {
   CONDITIONAL_TP(smacc2_state_onRuntimeConfigure_start, state_name);
 }
 
-void TRACEPOINT(smacc2_state_onRuntimeConfigure_end, const char * state_name)
+void TRACETOOLS_TRACEPOINT(smacc2_state_onRuntimeConfigure_end, const char * state_name)
 {
   CONDITIONAL_TP(smacc2_state_onRuntimeConfigure_end, state_name);
 }
 
-void TRACEPOINT(smacc2_state_onEntry_start, const char * state_name)
+void TRACETOOLS_TRACEPOINT(smacc2_state_onEntry_start, const char * state_name)
 {
   CONDITIONAL_TP(smacc2_state_onEntry_start, state_name);
 }
 
-void TRACEPOINT(smacc2_state_onEntry_end, const char * state_name)
+void TRACETOOLS_TRACEPOINT(smacc2_state_onEntry_end, const char * state_name)
 {
   CONDITIONAL_TP(smacc2_state_onEntry_end, state_name);
 }
 
-void TRACEPOINT(smacc2_state_onExit_start, const char * state_name)
+void TRACETOOLS_TRACEPOINT(smacc2_state_onExit_start, const char * state_name)
 {
   CONDITIONAL_TP(smacc2_state_onExit_start, state_name);
 }
 
-void TRACEPOINT(smacc2_state_onExit_end, const char * state_name)
+void TRACETOOLS_TRACEPOINT(smacc2_state_onExit_end, const char * state_name)
 {
   CONDITIONAL_TP(smacc2_state_onExit_end, state_name);
 }
 
-void TRACEPOINT(
+void TRACETOOLS_TRACEPOINT(
   smacc2_client_behavior_on_entry_start, const char * state_name, const char * orthogonal_name,
   const char * client_behavior_name)
 {
@@ -79,7 +79,7 @@ void TRACEPOINT(
     smacc2_client_behavior_on_entry_start, state_name, orthogonal_name, client_behavior_name);
 }
 
-void TRACEPOINT(
+void TRACETOOLS_TRACEPOINT(
   smacc2_client_behavior_on_entry_end, const char * state_name, const char * orthogonal_name,
   const char * client_behavior_name)
 {
@@ -87,7 +87,7 @@ void TRACEPOINT(
     smacc2_client_behavior_on_entry_end, state_name, orthogonal_name, client_behavior_name);
 }
 
-void TRACEPOINT(
+void TRACETOOLS_TRACEPOINT(
   smacc2_client_behavior_on_exit_start, const char * state_name, const char * orthogonal_name,
   const char * client_behavior_name)
 {
@@ -95,7 +95,7 @@ void TRACEPOINT(
     smacc2_client_behavior_on_exit_start, state_name, orthogonal_name, client_behavior_name);
 }
 
-void TRACEPOINT(
+void TRACETOOLS_TRACEPOINT(
   smacc2_client_behavior_on_exit_end, const char * state_name, const char * orthogonal_name,
   const char * client_behavior_name)
 {
