c53489958f73ffe4971cdacdd8d40e591c227f0b
pabloinigoblasco <pablo@ibrobotics.com>
2025-09-10 23:58:08 +0200

    missing

--- smacc2_client_library/keyboard_client/include/keyboard_client/components/cp_keyboard_listener_1.hpp
+++ smacc2_client_library/keyboard_client/include/keyboard_client/components/cp_keyboard_listener_1.hpp
@@ -14,7 +14,7 @@
 
 #pragma once
 
-#include <smacc2/client_base_components/cp_topic_subscriber.hpp>
+#include <smacc2/client_core_components/cp_topic_subscriber.hpp>
 #include <smacc2/introspection/introspection.hpp>
 #include <smacc2/smacc.hpp>
 
